a9a7277c8eed938c718c5bf9221265ec30a4f5b5
Anurag Pal <anuragpal2004@gmail.com>
2024-01-04 16:55:48 +0530

    Project structure and basic routing done

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import AuthLayout from './_auth/AuthLayout'
+import SigninForm from './_auth/forms/SignInForm'
+import SignupForm from './_auth/forms/SignupForm'
+import RootLayout from './_root/RootLayout'
+import { Home } from './_root/pages'
+import './global.css'
+import { Routes,Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>App</div>
+    <main className='flex h-screen '>
+        <Routes>
+            {/* public routes */}
+            <Route element={<AuthLayout/>}>
+                <Route path='/sign-in' element={<SigninForm/>} />
+                <Route path='/sign-up' element={<SignupForm/>} />
+            </Route>
+
+
+            {/* private routes */}
+            <Route element={<RootLayout/>}>
+                <Route index element={<Home/>} />
+            </Route>
+        </Routes>
+    </main>
   )
 }
 
