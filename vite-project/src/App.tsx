a1655bab61bc71b77fd14802d373547c19460993
Anurag Pal <anuragpal2004@gmail.com>
2024-01-05 12:38:10 +0530

    Creating user functionality

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,9 +1,9 @@
 import AuthLayout from './_auth/AuthLayout'
-import SigninForm from './_auth/forms/SignInForm'
+import SigninForm from './_auth/forms/SigninForm'
 import SignupForm from './_auth/forms/SignupForm'
 import RootLayout from './_root/RootLayout'
 import { Home } from './_root/pages'
-import './global.css'
+import './globals.css'
 import { Routes,Route } from 'react-router-dom'
 
 const App = () => {
